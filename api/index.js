45125d88e0cf0c6486632bbe66a94bee5a39f36e
mcenberci <mcenberci@gmail.com>
2024-05-20 21:32:11 +0300

    try

--- api/index.js
+++ api/index.js
@@ -3,17 +3,17 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fs = require('fs');
-const englishDictionary = require("./dummyDictionary/dictionary.json");
+const englishDictionary = require("../dummyDictionary/dictionary.json");
 app.use(cors());
 app.use(bodyParser.json());
-app.post("/en", (req, res) => {
+app.post("/api/en", (req, res) => {
   var {value} = req.body
   value = value.toLocaleLowerCase("en");
   if(englishDictionary[value])res.json(true)
     else res.json(false)
 })
 
-app.post("/tr", (req, res) => {
+app.post("/api/tr", (req, res) => {
   var {value} = req.body
   value = value.toLocaleLowerCase("tr");
   fs.readFile('./dummyDictionary/turkishDictionary.txt', 'utf8', (err, data) => {
